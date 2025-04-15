b4f62fa3f4ee361afecfcb035a79ab9ee5110be6
keshva55 <keshavpatelandroid@gmail.com>
2025-04-15 08:34:42 +0530

    ok

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,19 @@
+// Primitive DataType
+
+// 7 type: String ,Number,Boolean,null,undefined,Symbol, BigInt
+// Reference Type(Non Primitive)
+  //  Array,Objects,Functions,
+  const score = 100;
+  const scoreValue = 100.3
+
+  const id = Symbol('123')
+  const anotherID = Symbol('123');
+
+    const myFunction =   function(){
+    console.log("SKK");
+
+  }
+
+  console.log(myFunction());
+  console.log(typeof myFunction);
+  
