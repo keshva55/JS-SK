95de379acdc47acf193b511807a6784ec6454eca
keshva55 <keshavpatelandroid@gmail.com>
2025-04-16 09:19:28 +0530

    ok

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,19 +1,40 @@
 // Primitive DataType
-
 // 7 type: String ,Number,Boolean,null,undefined,Symbol, BigInt
 // Reference Type(Non Primitive)
   //  Array,Objects,Functions,
-  const score = 100;
-  const scoreValue = 100.3
+  // const score = 100;
+  // const scoreValue = 100.3
 
-  const id = Symbol('123')
-  const anotherID = Symbol('123');
+  // const id = Symbol('123')
+  // const anotherID = Symbol('123');
 
-    const myFunction =   function(){
-    console.log("SKK");
+  //   const myFunction =   function(){
+  //   console.log("SKK");
 
+  // }
+
+  // console.log(myFunction());
+  // console.log(typeof myFunction);
+
+// Memory///
+//Stack(Primitive), Heap(Non-Primitive)
+//stack me copy milta hai
+//Heap me ref milta hai
+let myWork = "skk";
+
+let anotherName = myWork;
+anotherName = "vkk"
+//console.log(myWork);
+console.log(myWork);
+console.log(anotherName);
+
+  let userOne = {
+      email : "sk@gmail.com",
+      upi: "sk@upi"
   }
+  let userTwo = userOne;
 
-  console.log(myFunction());
-  console.log(typeof myFunction);
-  
+  userTwo.email = "vk@gmail.com"
+
+  console.log(userOne.email);
+  console.log(userTwo.email);
