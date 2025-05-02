71d9a4be195b85fdb5866e16cadc5352e47b39a8
keshva55 <keshavpatelandroid@gmail.com>
2025-05-02 08:06:57 +0530

    Add initial implementation of string manipulation examples

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,10 @@
+const name = "Keshav"
+const repoCount = 50
+//String InterPolation Morden Lang way ``
+console.log(`Hello Team Ny name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Keshav');
+console.log(gameName[0]); 
+gameName = new String("vijay");
+console.log(gameName[0]); 
+
